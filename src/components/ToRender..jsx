9d5d667de11f0897e9f1014d00965cb6e9032788
noaa <noaalevyart@gmail.com>
2024-02-27 23:31:12 +0200

    initial1

---
+++ src/components/ToRender..jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const ToRender = () => {
+
+    const [name, setName] = useState('insert your name');
+
+    return <>
+
+        <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+        <p>your name is {name}</p>
+
+    </>
+}
