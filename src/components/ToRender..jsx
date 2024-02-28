164301cdaeafd3f8bb411b1283e28019c41bcfae
noaa <noaalevyart@gmail.com>
2024-02-28 22:44:25 +0200

    noa branch

--- src/components/ToRender..jsx
+++ src/components/ToRender..jsx
@@ -8,8 +8,5 @@ export const ToRender = () => {
 
         <input type="text" value={name} onChange={e => setName(e.target.value)}/>
         <p>your name is {name}</p>
-
-        <a>nbnbnbnbn</a>
-
     </>
 }
